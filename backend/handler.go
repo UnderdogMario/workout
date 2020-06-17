34a8abf81cee7b71f198f366665c17a968ec86ce
UnderdogMario <61845086+UnderdogMario@users.noreply.github.com>
2020-06-17 14:15:39 -0700

    finish login api

--- backend/handler.go
+++ backend/handler.go
@@ -14,11 +14,23 @@ func DefaultHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, "Default, Up And Running, Good Connection")
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func LoginHandler(writer http.ResponseWriter, request *http.Request) {
+	//fmt.Println(request)
+	setupResponse(&writer, request)
+	if (*request).Method == "OPTIONS" {
+		return
+	}
 	request.ParseForm()
 	var email string
 	var password string
 	for key, value := range request.Form {
+		fmt.Println(key, value)
 		if key == "email" {
 			email = value[0]
 		}
